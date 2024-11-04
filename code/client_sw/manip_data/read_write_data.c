8cd9affa11a169d2a512fbbb5b4dc3af90d4c3c5
Francesco Marinelli <90141448+framarin-GitHub@users.noreply.github.com>
2024-11-04 16:22:01 +0100

    add data manip functionalities

--- code/client_sw/manip_data/read_write_data.c
+++ code/client_sw/manip_data/read_write_data.c
@@ -13,17 +13,17 @@
 
 
 
-ts_data hour[60];
-ts_data day[24];
-ts_data month[30];
-ts_data year[12];
+ts_data* hour[60];
+ts_data* day[24];
+ts_data* month[30];
+ts_data* year[12];
 
 
 void synchroArrays(){
-  memset(hour, 0, sizeof(ts_data)*60);
-  memset(day, 0, sizeof(ts_data)*24);
-  memset(month, 0, sizeof(ts_data)*30);
-  memset(year, 0, sizeof(ts_data)*12);
+  memset(hour, 0, sizeof(ts_data*)*60);
+  memset(day, 0, sizeof(ts_data*)*24);
+  memset(month, 0, sizeof(ts_data*)*30);
+  memset(year, 0, sizeof(ts_data*)*12);
   FILE* f_h = fopen("./diagram_output/data/hour.temp", "r+");
   FILE* f_d = fopen("./diagram_output/data/day.temp", "r+");
   FILE* f_m = fopen("./diagram_output/data/month.temp", "r+");
@@ -40,7 +40,7 @@ void synchroArrays(){
       HANDLE_ERROR_ERRNO("error reading hour.temp\n");
     if(i > 59)
       HANDLE_ERROR_ERRNO("error reading hour.temp\n");
-    hour[i] = *createTsData(value_read, ts_read);
+    hour[i] = createTsData(value_read, ts_read);
     i++;
   } while(1);
 
@@ -56,7 +56,7 @@ void synchroArrays(){
       HANDLE_ERROR_ERRNO("error reading day.temp\n");
     if(i > 23)
       HANDLE_ERROR_ERRNO("error reading day.temp\n");
-    day[i] = *createTsData(value_read, ts_read);
+    day[i] = createTsData(value_read, ts_read);
     i++;
   } while(1);
 
@@ -72,7 +72,7 @@ void synchroArrays(){
       HANDLE_ERROR_ERRNO("error reading month.temp\n");
     if(i > 29)
       HANDLE_ERROR_ERRNO("error reading month.temp\n");
-    month[i] = *createTsData(value_read, ts_read);
+    month[i] = createTsData(value_read, ts_read);
     i++;
   } while(1);
   
@@ -88,7 +88,7 @@ void synchroArrays(){
       HANDLE_ERROR_ERRNO("error reading year.temp\n");
     if(i > 11)
       HANDLE_ERROR_ERRNO("error reading year.temp\n");
-    year[i] = *createTsData(value_read, ts_read);
+    year[i] = createTsData(value_read, ts_read);
     i++;
   } while(1);
   
@@ -102,25 +102,15 @@ void writeDataToFile(){
   FILE* f_d = fopen("./diagram_output/data/day.temp", "w+");
   FILE* f_m = fopen("./diagram_output/data/month.temp", "w+");
   FILE* f_y = fopen("./diagram_output/data/year.temp", "w+");
-  
-  
-      //dacapire
-  int flag = 0;
-  int i = 0;
-  do {  
-    flag = fprintf(f_h,"%ld %d\n", hour[i].timestamp, hour[i].value);
-    //if(flag == EOF)
-      //break;
-    //if(flag == 1)
-      //HANDLE_ERROR_ERRNO("error writing hour.temp\n");
-    if(i > 59)
-      break;
-      //HANDLE_ERROR_ERRNO("error reading hour.temp\n");
-    i++;
-  } while(1);
-
-  
-  
+    
+  for(int i = 0; i < 60; i++)
+    fprintf(f_h,"%ld %d\n", hour[i]->timestamp, hour[i]->value);    
+  for(int i = 0; i < 24; i++)
+    fprintf(f_d,"%ld %d\n", day[i]->timestamp, day[i]->value);
+  for(int i = 0; i < 30; i++)
+    fprintf(f_m,"%ld %d\n", month[i]->timestamp, month[i]->value);
+  for(int i = 0; i < 12; i++)
+    fprintf(f_y,"%ld %d\n", year[i]->timestamp, year[i]->value);
   
   fclose(f_h);
   fclose(f_d);
@@ -128,13 +118,71 @@ void writeDataToFile(){
   fclose(f_y);
 }
 
+void insertSortData(int type, ts_data* to_ins){
+  ts_data* temp1;
+  ts_data* temp2; 
+  switch(type){
+    case 0:
+      temp1 = hour[0];
+      temp2 = to_ins;
+      for(int i = 0; i < 60; i++){
+        hour[i] = temp2;
+        temp2 = temp1;
+        if(i == 59)
+          break;
+        temp1 = hour[i+1];
+      }
+      if(temp1->timestamp != 0)
+        deleteTsData(temp1);
+      break;
+    case 1:
+      temp1 = day[0];
+      temp2 = to_ins;
+      for(int i = 0; i < 24; i++){
+        day[i] = temp2;
+        temp2 = temp1;
+        if(i == 23)
+          break;
+        temp1 = day[i+1];
+      }
+      if(temp1->timestamp != 0)
+        deleteTsData(temp1);
+      break;
+    case 2:
+      temp1 = month[0];
+      temp2 = to_ins;
+      for(int i = 0; i < 30; i++){
+        month[i] = temp2;
+        temp2 = temp1;
+        if(i == 29)
+          break;
+        temp1 = month[i+1];
+      }
+      if(temp1->timestamp != 0)
+        deleteTsData(temp1);
+      break;
+    case 3:  
+      temp1 = year[0];
+      temp2 = to_ins;
+      for(int i = 0; i < 12; i++){
+        year[i] = temp2;
+        temp2 = temp1;
+        if(i == 11)
+          break;
+        temp1 = year[i+1];
+      }
+      if(temp1->timestamp != 0)
+        deleteTsData(temp1);
+      break;
+  }
+} 
 
 void initData(){
   synchroArrays();
   printf("data initialized ...\n");
 }
 
-ts_data* getData(int type){
+ts_data** getArrayData(int type){
   switch(type){
     case 0:
       return hour;
@@ -145,5 +193,5 @@ ts_data* getData(int type){
     case 3:
       return year;
   }
-  return (ts_data*)-1;
+  return (ts_data**)-1;
 }
--- code/client_sw/manip_data/read_write_data.h
+++ code/client_sw/manip_data/read_write_data.h
@@ -1,3 +1,5 @@
 #pragma once
 
 void initData();
+
+ts_data** getArrayData(int type);
